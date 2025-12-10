0fbee8c32f2c6f54c71f6f0643994f15fa9118d3
immutablehub <i72085771@gmail.com>
2025-12-10 19:34:19 +0530

    Create gitbundler.js

---
+++ core/individualunits/gitbundler.js
@@ -0,0 +1,26 @@
+import { execSync } from "child_process";
+import * as crypto from "crypto"
+import path from "path";
+
+
+function gitBundler(FOLDER){
+
+
+    let dynamicstring=crypto.randomUUID()
+    let bpath=`${dynamicstring}.history.bundle`
+
+    if(fs.existsSync(bpath)) {
+        console.log("path already exists")
+    }
+    else {
+
+      const bundlePath = path.join(FOLDER,bpath);
+      execSync(`git bundle create ${path.basename(bundlePath)} --all`, {
+          cwd: FOLDER,
+          stdio: 'inherit'
+        });
+      const bundleSize = fs.statSync(bundlePath).size;
+      console.log(`✓ Bundle created: ${(bundleSize / 1024 / 1024).toFixed(2)} MB`);
+      console.log(`  Location: ${bundlePath}\n`);
+    }
+}
