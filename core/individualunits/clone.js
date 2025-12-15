e4e8e026ef0c2f6bd64879c06f3116c9618847b4
immutablehub <i72085771@gmail.com>
2025-12-15 20:42:44 +0530

    Create clone.js

---
+++ core/individualunits/clone.js
@@ -0,0 +1,43 @@
+
+async function Clone(folder){
+
+    
+    const targetManifestId= fs.readFileSync(FILE_TO_STORE_LOGIN, 'utf8');
+    const doc = await coll.findOne({
+        "owner": "system",
+        "manifests.id": targetManifestId 
+    });
+    if(doc) {
+
+        let uploads=null
+        let bundle=null
+        let manifests=doc.manifests
+    
+
+    for(let obj of  manifests){
+        
+        let dbfolder=String(obj.folder)
+
+          if(obj.id==targetManifestId && dbfolder==folder){
+              console.log(obj.folder)
+              uploads=obj.uploads
+           }
+
+    
+
+        }
+        console.log(uploads)
+  
+    for(let obj of uploads){
+
+        let name=obj.name
+        const isIncluded = name.includes(".history.bundle");
+        if(isIncluded) {
+
+                bundle=obj.cid
+
+          }}
+
+      await getFile(bundle)
+
+  }}
