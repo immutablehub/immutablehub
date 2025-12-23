fc2cedf9b30b2fda79b908d366c75cae1fe8fba2
immutablehub <i72085771@gmail.com>
2025-12-23 22:36:54 +0530

    Update push.js

--- core/individualunits/push.js
+++ core/individualunits/push.js
@@ -1,23 +1,45 @@
 
-async function Push(FOLDER_TO_UPLOAD) {
+async function Push(FOLDER_TO_UPLOAD,pinata,client) {
 
      try {
 
-          let uploads=[]
-          let files=dirtoFileArray(FOLDER_TO_UPLOAD)
-          for (let file of files) {
-              const upload=await pinata.upload.public.file(file)
-              uploads.push(upload)
-          }
-          console.log(uploads)
-          const data = fs.readFileSync(FILE_TO_STORE_LOGIN, 'utf8');
-          let meta={"id":data,"folder":FOLDER_TO_UPLOAD,"uploads":uploads}
-          await coll.findOneAndUpdate(
-              {"owner":"system"},
-              { $push: {manifests: meta } })
-      }catch(e){
-       
-       console.log(e)
+
+        const db = client.db("ihub_db");
+        const coll = db.collection("ihub_col");
+
+        let uploads=[]
+        const absolutePath = path.resolve(FOLDER_TO_UPLOAD);
+        let files=dirtoFileArray(absolutePath)
+
+        for (let file of files) {
+
+            const upload=await pinata.upload.public.file(file)
+            uploads.push(upload)
+        }
+
+
+        console.log(uploads)
+        const data = fs.readFileSync(FILE_TO_STORE_LOGIN, 'utf8');
+        const lastpath = path.basename(absolutePath);
+
+        let meta={"id":data,"folder":lastpath,"uploads":uploads,"is_latest":true}
+
+
+        let docalreadyexists=await manifestExists(data,lastpath,client)
+        if (docalreadyexists){
+             await deleteManifest(data,lastpath,client)
+        }
+
+        await coll.findOneAndUpdate(
+            {"owner":"system"},
+            {
+            $push: {
+            manifests: meta
+            }})
+     }
+     catch(e){
+
+      console.log(e)
       
      }
 
