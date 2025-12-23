02753774b6411d8cdea2766fe166d3df08f7ee54
immutablehub <i72085771@gmail.com>
2025-12-23 22:29:16 +0530

    Update and rename filefetcher.js to getfile.js

--- core/individualunits/getfile.js
+++ core/individualunits/getfile.js
@@ -1,6 +1,5 @@
-//IPFS decentralized immutable storage
 
-async function getFile(cid) {
+async function getFile(folder,cid,pinata) {
   
     const result = await pinata.gateways.public.get(cid)
     const Data = result.data;
@@ -8,11 +7,27 @@ async function getFile(cid) {
     const arrayBuffer = await Data.arrayBuffer()
     const buffer = Buffer.from(arrayBuffer);
     console.log(buffer)
+
+  
+    deleteFilesWithExtension(".history.bundle",".")
+
+
     let dynamicstring=crypto.randomUUID()
     let shortID = dynamicstring.substring(0, 5)
     let bpath=`${shortID}.history.bundle`
+
     fs.writeFileSync(`${bpath}`,buffer);
-    execSync(`git clone ${bpath}`, {
+
+    const absolutePath  = path.resolve(folder);
+
+     if (fs.existsSync(absolutePath)){
+      fs.rmSync(absolutePath, { recursive: true, force: true });
+     }
+
+
+    fs.mkdirSync(absolutePath, { recursive: true });
+
+    execSync(`git clone ${bpath} ${absolutePath}`, {
       cwd: ".",
       stdio: 'inherit'
     });
