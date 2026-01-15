5c42cebe984163f85981a66cbd00b812637cb81e
immutablehub <i72085771@gmail.com>
2026-01-15 23:11:31 +0530

    Update push.js

--- core/individualunits/push.js
+++ core/individualunits/push.js
@@ -1,23 +1,43 @@
-
-async function Push(FOLDER_TO_UPLOAD,pinata,client) {
+async function Push(FOLDER_TO_UPLOAD,pinata,client,mcp,prompt) {
 
      try {
 
+        let db = client.db("ihub_db");
+        let coll = db.collection("ihub_col");
 
-        const db = client.db("ihub_db");
-        const coll = db.collection("ihub_col");
+        if (mcp ==true){
 
-        let uploads=[]
+            db = client.db("ihub_db");
+            coll = db.collection("mcp_registry");
+
+        }
+        else if(prompt ==true){
+            db = client.db("ihub_db");
+            coll = db.collection("prompt_comp");
+        }
+
+        
+
+        //let uploads=[]
         const absolutePath = path.resolve(FOLDER_TO_UPLOAD);
         let files=dirtoFileArray(absolutePath)
 
+/*
         for (let file of files) {
 
             const upload=await pinata.upload.public.file(file)
             uploads.push(upload)
         }
+*/
+        
 
 
+        const limit = pLimit(5); //  5 concurrent uploads
+        const uploadPromises = files.map(file =>
+          limit(() => pinata.upload.public.file(file))
+        );
+
+        const uploads = await Promise.all(uploadPromises);
         console.log(uploads)
         const data = fs.readFileSync(FILE_TO_STORE_LOGIN, 'utf8');
         const lastpath = path.basename(absolutePath);
@@ -27,7 +47,8 @@ async function Push(FOLDER_TO_UPLOAD,pinata,client) {
 
         let docalreadyexists=await manifestExists(data,lastpath,client)
         if (docalreadyexists){
-             await deleteManifest(data,lastpath,client)
+             //await deleteManifest(data,lastpath,client,mcp,prompt)
+             await deactivateOlderVersions(data,lastpath,mcp,prompt)
         }
 
         await coll.findOneAndUpdate(
